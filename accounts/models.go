a2754fe1e3e8a830e956c5b0e69e68796eb1e5f4
Anurag Maurya <anurag@carret.in>
2026-01-02 18:36:06 +0530

    complex datatype working postgres only

--- accounts/models.go
+++ accounts/models.go
@@ -1,6 +1,8 @@
 package accounts
 
 import (
+	"time"
+
 	"github.com/anuragcarret/djang-drf-go/contrib/auth"
 	"github.com/anuragcarret/djang-drf-go/orm/models"
 )
@@ -16,6 +18,20 @@ type Account struct {
 
 func (a *Account) TableName() string { return "accounts" }
 
+type ComplexData struct {
+	models.Model
+	Status         string                 `drf:"status;max_length=20;default=active"`
+	Score          float64                `drf:"score;type=numeric"`
+	Tags           []string               `drf:"tags"`
+	Metadata       map[string]interface{} `drf:"metadata"`
+	ExternalID     string                 `drf:"external_id;type=uuid;unique"`
+	IsProcessed    bool                   `drf:"is_processed;default=false"`
+	ActivationDate time.Time              `drf:"activation_date;type=date;null"`
+}
+
+func (c *ComplexData) TableName() string { return "complex_data" }
+
 func init() {
 	models.RegisterModel("accounts", &Account{})
+	models.RegisterModel("accounts", &ComplexData{})
 }
