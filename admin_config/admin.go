e4d55b063ccc39ef82abb1ed24bf6e0c4ac97cb9
Anurag Maurya <anurag@carret.in>
2026-01-02 22:37:16 +0530

    delete and view working

--- admin_config/admin.go
+++ admin_config/admin.go
@@ -6,6 +6,7 @@ import (
 
 	framework_admin "github.com/anuragcarret/djang-drf-go/admin"
 	"github.com/anuragcarret/djang-drf-go/contrib/auth"
+	"github.com/anuragcarret/djang-drf-go/orm/queryset"
 )
 
 func init() {
@@ -20,6 +21,19 @@ func init() {
 
 	framework_admin.Register[*posts.Post](framework_admin.DefaultSite, &framework_admin.ModelAdmin{
 		ListDisplay: []string{"Content", "Created"},
+		Actions: []framework_admin.AdminAction{
+			{
+				Name:  "log_to_console",
+				Label: "Log to Console",
+				Handler: func(qs *queryset.QuerySet[*posts.Post], ids []uint64) (string, error) {
+					println("LOGGING SELECTED POSTS:", len(ids))
+					for _, id := range ids {
+						println("  - Post ID:", id)
+					}
+					return "Logged to console", nil
+				},
+			},
+		},
 	})
 
 	framework_admin.Register[*posts.Comment](framework_admin.DefaultSite, &framework_admin.ModelAdmin{
